26fbc3bc56d0b04802dc8f5bfc081ab5ec57a9b2
Cezar Mocan <mocancezar@gmail.com>
2019-11-11 14:57:17 -0500

    Mobile styling

--- components/Navigation.js
+++ components/Navigation.js
@@ -27,68 +27,12 @@ class Navigation extends React.Component {
       const { toggleMouseTracker } = this.props
       toggleMouseTracker(true)
     }
-    renderAboutPageNav() {
-        return (
-            <>
-                <div className="nav-container nav-top-right interactive" onClick={this.onAboutCloseClick}>
-                    <img src="static/icons/nav_X.svg"/>
-                </div>
-                <div className="nav-container nav-bottom-right interactive fadeout-after">
-                    <img src="static/icons/2_nav_V.svg"/>
-                </div>
-                <div className="nav-container nav-bottom-center">
-                    <h4>© Anthony Gagliardi, 2019.</h4>
-                </div>
-            </>
-        )
-    }
-    renderMainPageNav() {
-        const { currentProjectId, getCurrentProjectMetadata } = this.props
-        const { title, year } = getCurrentProjectMetadata()
-        return (
-            <>
-                <div className="nav-container nav-bottom-right interactive"
-                  onClick={this.onNavigateNextClick}
-                  onMouseEnter={this.onNavigationMouseEnter}
-                  onMouseLeave={this.onNavigationMouseLeave}
-                >
-                    <img src="static/icons/2_nav_V.svg"/>
-                </div>
-                <div className="nav-container nav-bottom-left interactive"
-                  onClick={this.onNavigatePrevClick}
-                  onMouseEnter={this.onNavigationMouseEnter}
-                  onMouseLeave={this.onNavigationMouseLeave}
-                >
-                    <img src="static/icons/2_nav_A.svg"/>
-                </div>
-                <div className="nav-container nav-top-right interactive"
-                  onClick={this.onAboutClick}
-                  onMouseEnter={this.onNavigationMouseEnter}
-                  onMouseLeave={this.onNavigationMouseLeave}
-                >
-                    <img src="static/icons/2_nav_G.svg"/>
-                </div>
-                <div className="nav-container nav-top-left interactive"
-                  onMouseEnter={this.onNavigationMouseEnter}
-                  onMouseLeave={this.onNavigationMouseLeave}
-                >
-                    { title && year && <h3>{title}, {year}</h3> }
-                    { title && !year && <h3>{title} </h3> }
-                    { !title && <h3></h3> }
-                </div>
-
-                <div className="nav-container nav-bottom-center">
-                    <h4>© Anthony Gagliardi, 2019.</h4>
-                </div>
-            </>
-        )
-    }
     render() {
         const { isAboutPageOpen } = this.props
         const { currentProjectId, getCurrentProjectMetadata } = this.props
         const { title, year } = getCurrentProjectMetadata()
-        const brClassnames = classnames({'nav-container': true, 'nav-bottom-right': true, 'interactive': !isAboutPageOpen, visible: !isAboutPageOpen })
-        const blClassnames = classnames({'nav-container': true, 'nav-bottom-left': true, 'interactive': !isAboutPageOpen, visible: !isAboutPageOpen })
+        const brClassnames = classnames({'nav-container': true, 'nav-next': true, 'interactive': !isAboutPageOpen, visible: !isAboutPageOpen })
+        const blClassnames = classnames({'nav-container': true, 'nav-previous': true, 'interactive': !isAboutPageOpen, visible: !isAboutPageOpen })
         const trClassnames = classnames({'nav-container': true, 'nav-top-right': true, 'interactive': false, visible: false })
         const trAboutClassnames = classnames({'nav-container': true, 'nav-top-right': true, 'interactive': isAboutPageOpen, 'close-image': true, visible: isAboutPageOpen })
         const tlClassnames = classnames({'nav-container': true, 'nav-top-left': true, 'interactive': !isAboutPageOpen, visible: !isAboutPageOpen, 'title-corner': true })
@@ -137,8 +81,6 @@ class Navigation extends React.Component {
                 </div>
             </>
         )
-        // if (!isAboutPageOpen) return this.renderMainPageNav()
-        // else return this.renderAboutPageNav()
     }
 }
 
