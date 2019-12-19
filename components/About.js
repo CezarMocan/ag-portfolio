2733ad648a8c4875ec09e163d13626b4973c7660
Cezar Mocan <mocancezar@gmail.com>
2019-12-19 12:59:16 +0200

    Aligning items; consistency

--- components/About.js
+++ components/About.js
@@ -4,16 +4,6 @@ import PortableBlockContent from '@sanity/block-content-to-react'
 import { withMainContext } from '../context/MainContext'
 
 class About extends React.Component {
-    state = {
-        currentHoverId: null
-    }
-    onMouseEnter = (projectId) => (e) => {
-        this.setState({ currentHoverId: projectId })        
-    }
-    onMouseLeave = (projectId) => (e) => {
-        if (this.state.currentHoverId == projectId)
-            this.setState({ currentHoverId: null })        
-    }
     onProjectClick = (projectId) => (e) => {
         const { navigateToProjectId, toggleMouseTracker } = this.props
         toggleMouseTracker(true)
@@ -25,7 +15,6 @@ class About extends React.Component {
             'about-container': true,
             'visible': isAboutPageOpen 
         })
-        const { currentHoverId } = this.state
         return (
             <div className={cls}>
                 <div className="nav-about-top-left">
@@ -34,24 +23,11 @@ class About extends React.Component {
                 
                 <p>
                     { projects && projects.map((p, index) => {
-                        const rgba = p.color.rgb
-                        const hoverCls = classnames({
-                            "project-link-hover": true,
-                            "visible": currentHoverId == p.id
-                        })
                         return (
-                            <div className="project-link-container"
-                                onMouseEnter={this.onMouseEnter(p.id)}
-                                onMouseLeave={this.onMouseLeave(p.id)}
+                            <div className="project-link-container interactive"
                                 onClick={this.onProjectClick(p.id)}
                             >
                                 <span className="project-link">
-                                    &emsp;{index + 1}. {p.title}&emsp;<br/>
-                                </span>
-                                <span 
-                                    className={hoverCls}
-                                    style={{borderBottom: `2px solid rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a})`}} 
-                                >
                                     &emsp;{index + 1}. {p.title}&emsp;<br/>
                                 </span>
                             </div>
