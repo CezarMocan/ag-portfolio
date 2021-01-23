badc773bc3f34f4afe4f119210f3888b11a9866c
Cezar Mocan <mocancezar@gmail.com>
2021-01-23 16:20:07 -0500

    new favicon

--- components/Head.js
+++ components/Head.js
@@ -11,12 +11,9 @@ class HeadComponent extends React.Component {
         <title>{usedTitle}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
         <meta name="description" content="Anthony Gagliardi is an architect and co-founder of Almost Studio. He received a M.Arch from the Yale School of Architecture, where he was the Yansong Ma Scholar, and a B.S. in Architecture with Distinction from The Ohio State University."/>
-        {/* <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/> */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-4-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon-4-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-4-16x16.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-g2-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon-g2-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-g2-16x16.png"/>
       </Head>
     )
   }
