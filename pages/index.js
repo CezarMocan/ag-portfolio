bd9f0213f22a136f169841649e443562244b02b7
Cezar Mocan <mocancezar@gmail.com>
2021-01-23 19:40:33 -0500

    gid

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,12 @@ import ProjectView from '../components/ProjectView'
 import { withPageRouter } from '../modules/withPageRouter'
 import GA4React from 'ga-4-react'
 
-const GA_ID = 'G-RX4SB2CJ99'
+const GA_ID = 'G-DK0YGTXHV9'
 const ga4react = new GA4React(GA_ID);
 
 class Index extends React.Component {
     componentDidMount() {
       ga4react.initialize().then((ga4) => {
-        // ga4.pageview(window.location.pathname)
         ga4.gtag('event','pageview','path') // or your custom gtag event
       },(err) => {
         console.error(err)
